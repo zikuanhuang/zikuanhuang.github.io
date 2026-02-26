de3e39575e7e93570638b78688282b6c3a1ca276
zikuanhuang <126777482+zikuanhuang@users.noreply.github.com>
2026-02-26 10:06:20 +0000

    Deploying to gh-pages from @ zikuanhuang/zikuanhuang.github.io@2ac975a55ab36e35c0a5c26e9937df5592343a72 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -514,25 +514,11 @@ ninja.data = [{
           window.open("/assets/pdf/cv_pdf.pdf", "_blank");
         },
       },{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=EDJSTAcAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
