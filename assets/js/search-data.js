1449ae27bbaa6d93202a1ddcff53fc8ba6e0c142
zikuanhuang <126777482+zikuanhuang@users.noreply.github.com>
2026-02-26 13:49:03 +0000

    Deploying to gh-pages from @ zikuanhuang/zikuanhuang.github.io@735403241f30fb049761c77ad2dc5d9bd52bc7b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,12 +45,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-joined-shanghai-qizhi-institute-as-a-researcher",
+      },{id: "news-i-joined-shanghai-qizhi-institute-as-a-researcher",
           title: 'I joined Shanghai Qizhi Institute as a researcher!',
           description: "",
           section: "News",},{
